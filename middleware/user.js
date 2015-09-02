0c7e88bb4f6ac2b4baaad5731e44d96eef657927
fiiv <mike.timofiiv@gmail.com>
2015-09-02 13:59:01 +0200

    changes

---
+++ middleware/user.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function(req, res, next) {
+  if (!req.bearerToken) return next();
+
+  var User = DS[Instance.scopable.model];
+  var query = User.findOne({ _id: req.bearerToken.user });
+
+  query.lean().exec(function(err, user) {
+    if (err) return next(err);
+
+    req.user = user;
+    return next();
+  });
+};
