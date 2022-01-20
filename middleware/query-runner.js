8ff52ab69811603d59395ff0e85a672f596ae20a
devboell <devboell@gmail.com>
2022-01-20 10:50:42 +0100

    upgrading to mogoose 6

--- middleware/query-runner.js
+++ middleware/query-runner.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
   query-runner.js
@@ -8,11 +8,9 @@
 */
 
 module.exports = {
-
-  inject: [ 'DS' ],
+  inject: ["DS"],
 
   middleware: function queryRunnerMiddleware(DS) {
-
     return function queryRunner(req, res, next) {
       const Model = DS[req.controller.model];
       req.rawResources = {};
@@ -23,15 +21,18 @@ module.exports = {
       if (req.params.id) {
         query = Model.findById(req.params.id);
 
-        if (req.method === 'GET') query = query.lean(req.controller.spec ? req.controller.spec.leanQuery : true);
-      } else if (!req.params.id && req.method === 'GET') {
+        if (req.method === "GET")
+          query = query.lean(
+            req.controller.spec ? req.controller.spec.leanQuery : true
+          );
+      } else if (!req.params.id && req.method === "GET") {
         query = Model.find(req.searchParams);
-        count = Model.count(req.searchParams);
+        count = Model.countDocuments(req.searchParams);
 
         if (req.query.page) {
           const perPage = Number(req.query.perPage || 20);
 
-          query = query.skip((Number(req.query.page) * perPage) - perPage);
+          query = query.skip(Number(req.query.page) * perPage - perPage);
           query = query.limit(perPage);
         } else {
           query = query.limit(Number(req.query.limit || 20));
@@ -39,12 +40,18 @@ module.exports = {
 
         if (req.query.order) query = query.sort(req.query.order);
         if (req.query.sort) query = query.sort(req.query.sort);
-        if (req.method === 'GET') query = query.lean(req.controller.spec ? req.controller.spec.leanQuery : true);
+        if (req.method === "GET")
+          query = query.lean(
+            req.controller.spec ? req.controller.spec.leanQuery : true
+          );
       } else {
         return next();
       }
 
-      if (req.includeRelations && Object.keys(req.includeRelations).length > 0) {
+      if (
+        req.includeRelations &&
+        Object.keys(req.includeRelations).length > 0
+      ) {
         for (let r in req.includeRelations) {
           const relation = req.includeRelations[r];
 
@@ -67,7 +74,5 @@ module.exports = {
         });
       });
     };
-
-  }
-
+  },
 };
